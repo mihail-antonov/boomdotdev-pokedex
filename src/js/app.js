1378b686bec7b6c92ed918798fea0d42a279aa3e
Mihail Antonov <mihail.d.antonov@gmail.com>
2023-01-20 18:20:52 +0200

    Pokemon API fetch task - get first 10 pokemons and list them on the browser

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,45 @@
-import "../scss/app.scss";
+import "../scss/app.scss"
+
+let pokemonsArray = null
+const ul = document.querySelector("ul")
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
-});
+  fetchPokemons()
+})
+
+const fetchPokemons = () => {
+
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10').then(checkStatus).then(toJSON).then(data => {
+
+    pokemonsArray = data.results
+  }).then(appendPokemons)
+}
+
+const appendPokemons = () => {
+
+  pokemonsArray.forEach(pokemon => {
+
+    const list = document.createElement('li')
+
+    list.innerHTML = pokemon.name
+
+    ul.append(list)
+  })
+}
+
+const checkStatus = (response) => {
+
+  if (response.status >= 200 && response.status < 300) {
+
+    return Promise.resolve(response)
+  } else {
+
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+const toJSON = (response) => {
+
+  return response.json()
+}
